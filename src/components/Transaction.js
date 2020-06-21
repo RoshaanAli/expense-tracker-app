7e3d5dc9705798d79926c4f3c26b26a1a38eb125
RoshaanAli <roshaanalishah97@gmail.com>
2020-06-21 13:00:01 -0700

    Expense tracker app using contextAPI

---
+++ src/components/Transaction.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+
+export const Transaction = ({ transactions }) => {
+    const sign = transactions.amount < 0 ? '-' : '+';
+    const { deleteTransaction } = useContext(GlobalContext);
+    return (
+        <>
+            <li className={transactions.amount < 0 ? 'minus' : 'plus'}>
+                {transactions.text} <span>{sign}${Math.abs(transactions.amount)}</span>
+                <button onClick={() => deleteTransaction(transactions.id)} className="delete-btn">X</button>
+            </li>
+        </>
+    )
+}
